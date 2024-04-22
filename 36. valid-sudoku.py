4caace183f0c0bed4a091501539e616bf200b6a5
Yanyucheng <673816542@qq.com>
2024-04-22 17:24:28 +0800

    update in 2024.4.22

---
+++ 36. valid-sudoku.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row=defaultdict(set)
+        col=defaultdict(set)
+        square=defaultdict(set)
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] =='.':
+                    continue
+                if board[i][j] in row[i] or \
+                    board[i][j] in col[j] or \
+                    board[i][j] in square[(i//3,j//3)]:
+                    return False
+                row[i].add(board[i][j])
+                col[j].add(board[i][j])
+                square[(i//3,j//3)].add(board[i][j])
+                
+        return True
